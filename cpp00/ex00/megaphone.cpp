9ae459287f881e2a20049f0c7dacf16241e18665
fclivaz42 <fclivaz99@gmail.com>
2023-09-09 02:26:36 +0200

    ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fclivaz <fclivaz@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 02:13:28 by fclivaz           #+#    #+#             */
+/*   Updated: 2023/09/09 02:21:19 by fclivaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <cstring>
+#include <iostream>
+
+int main(int ac, char *av[])
+{
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int x = 1; x < ac; ++x)
+	{
+		for (int i = 0; i < strlen(av[x]); ++i)
+		{
+			std::cout << (char)toupper(av[x][i]);
+		}
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
