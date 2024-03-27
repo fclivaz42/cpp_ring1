779dcaadff9b4b66f39a87b6baddc67b3e62e1ea
fclivaz42 <fclivaz@pm.me>
2024-03-28 00:40:38 +0100

    ex0203

---
+++ cpp01/ex02/srcs/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fclivaz <fclivaz@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 22:58:21 by fclivaz           #+#    #+#             */
+/*   Updated: 2024/03/27 23:11:18 by fclivaz          ###    LAUSANNE.CH      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+int main (void)
+{
+	std::string		str = "HI THIS IS BRAIN!!!!!!!";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "Printing Addresses\n\n";
+
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << "\nPrinting Values\n\n";
+	
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return 0;
+}
