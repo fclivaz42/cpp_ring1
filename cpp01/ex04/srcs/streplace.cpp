abe7be8cc4dc642f556ba51aa347fdc8f357504a
fclivaz42 <fclivaz@pm.me>
2024-03-30 17:25:10 +0100

    yeah

--- cpp01/ex04/srcs/streplace.cpp
+++ cpp01/ex04/srcs/streplace.cpp
@@ -6,18 +6,28 @@
 /*   By: fclivaz <fclivaz@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 18:31:51 by fclivaz           #+#    #+#             */
-/*   Updated: 2024/03/29 18:35:51 by fclivaz          ###    LAUSANNE.CH      */
+/*   Updated: 2024/03/30 17:16:47 by fclivaz          ###    LAUSANNE.CH      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/streplace.hpp"
+#include <string>
 
 int main (int ac, char *av[])
 {
-	if (ac < 3){
-		std::cout << "Invalid amount of arguments.\nExptected: <filename> str_to_replace new_str\n";
+	std::string	path;
+	std::string	oldstr;
+	std::string	newstr;
+
+	if (ac != 4){
+		std::cout << "Invalid amount of arguments.\nExptected: <filename> \"str_to_replace\" \"new_str\"\n";
 		return 1;
 	}
-	std::cout << av << std::endl;
+
+	path = av[1];
+	oldstr = av[2];
+	newstr = av[3];
+	std::cout << "oldstr " << oldstr << "\n";
+	std::cout << "newstr " << newstr << "\n";
 	return 0;
 }
